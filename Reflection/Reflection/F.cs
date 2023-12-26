42c9a679a2d4a433d1b6f7cc74938de4d6ce971b
yuliya_goncharova <yuliagonch@mail.ru>
2023-12-26 15:20:28 +0500

    Тестовый проект с использованием Рефлексии

---
+++ Reflection/Reflection/F.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    public class F
+    {
+        public int i1;
+        public int i2;
+        public int i3;
+        public int i4;
+        public int i5;
+        public static F Get() => new F() { i1 = 1, i2 = 2, i3 = 3, i4 = 4, i5 = 5 };
+    }
+}
---
+++ Reflection/Reflection/Program.cs
@@ -0,0 +1,62 @@
+﻿
+using System;
+using System.Diagnostics;
+
+namespace Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            
+            F newF = F.Get();
+            Type typeF = typeof(F);
+
+            // Reflection serialize
+            stopwatch.Start();
+            string serializedS = SerializHelper.ReflectionSerialize(typeF, newF);
+            stopwatch.Stop();
+            Console.WriteLine($"ReflectionSerialize execution time in {SerializHelper.iterCount} iterations = {stopwatch.ElapsedMilliseconds} ms");
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            Console.WriteLine("Serialized string by reflection: ");
+            Console.WriteLine(serializedS);
+            stopwatch.Stop();
+            Console.WriteLine($"SerializString output time = {stopwatch.ElapsedMilliseconds} ms");
+
+            // Json serialize
+            stopwatch.Reset();
+            stopwatch.Start();
+            serializedS = SerializHelper.JsonSerialize(newF);
+            stopwatch.Stop();
+            Console.WriteLine("");
+            Console.WriteLine($"JsonSerialize execution time = {stopwatch.ElapsedMilliseconds} ms");
+
+            // Json deserialize
+            stopwatch.Reset();
+            stopwatch.Start();
+            var fObj = SerializHelper.JsonDesearialize<F>(serializedS);
+            stopwatch.Stop();
+            Console.WriteLine($"JsonDesearialize execution time = {stopwatch.ElapsedMilliseconds} ms");
+
+            // Csv serialize
+            stopwatch.Reset();
+            stopwatch.Start();
+            serializedS = SerializHelper.CsvSerialize(typeF, newF);
+            stopwatch.Stop();
+            Console.WriteLine("");
+            Console.WriteLine($"CsvSerialize execution time = {stopwatch.ElapsedMilliseconds} ms");
+
+            // Csv deserialize
+            stopwatch.Reset();
+            stopwatch.Start();
+            var obj = SerializHelper.CsvDeserialize(typeF, serializedS);
+            stopwatch.Stop();
+            Console.WriteLine($"CsvDeserialize execution time = {stopwatch.ElapsedMilliseconds} ms");
+
+            Console.ReadKey();
+        }
+    }
+}
